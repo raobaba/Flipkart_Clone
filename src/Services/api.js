341ad18cd9a5834270a2fd6b5d98912a9dca664b
Rajan Kumar <99542983+raobaba@users.noreply.github.com>
2023-06-15 12:42:10 +0530

    Update api.js

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = 'https://flipkart-server-o8zi.onrender.com';
 
 export const authenticateSignup = async (user) => {
   try {
