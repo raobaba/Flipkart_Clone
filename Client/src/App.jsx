2c73176ec3e6edacbc8e48fb27024fba1767c0b3
Rajan Kumar <raorajan9576@gmail.com>
2024-01-06 12:07:33 +0530

    Footer created successfullY

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Layouts/Footer/Footer";
 import Header from "./components/Layouts/Header/Header";
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   );
 }
