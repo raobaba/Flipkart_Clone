103e03c8dd9c280710322fe14aa1cf86e8d828fe
Rajan Kumar <raorajan9576@gmail.com>
2024-01-06 11:42:32 +0530

    header completed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import Header from "./components/Layouts/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header/>
     </>
   );
 }
