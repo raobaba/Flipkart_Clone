ea20f8bff49e2918bee3f1cf355e5eec1b19f767
Rajan Kumar <raorajan9576@gmail.com>
2024-01-07 10:00:13 +0530

    Login component created

--- Client/src/Routes/Routing.jsx
+++ Client/src/Routes/Routing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home.jsx";
+import Login from "../components/User/Login.jsx";
 
 function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   );
 }
