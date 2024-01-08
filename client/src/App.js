76b42cfa372a3057596643e1fb85fbe438ac2046
Rajan Kumar <raorajan9576@gmail.com>
2024-01-08 23:49:06 +0530

    redux added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Headers from './components/Layouts/Header/Header';
+import Footer from './components/Layouts/Footer/Footer';
+import Routing from './Routes/Routing';
+
+function App() {
+  return (
+    <>
+     <Headers/>
+     <Routing/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
